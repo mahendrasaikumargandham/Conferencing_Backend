a86165d7b941c23530ff202969f4bc3de1e0bc69
mahendrasaikumargandham <mahendragandham730@gmail.com>
2023-06-29 16:55:56 +0530

    Added Source Code

---
+++ public/script.js
@@ -0,0 +1,51 @@
+const videoGrid = document.getElementById("video-grid");
+const socket = io("/");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: 3030,
+});
+let myVideoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: false,
+  })
+  .then((stream) => {
+    (myVideoStream) => stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  });
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
